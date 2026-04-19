9cd5cbb3dd00b87df5155b7a2d15b6530b1de0a0
hashir008 <su92-bsdsm-s25-008@superior.edu.pk>
2026-04-20 02:42:43 +0500

    Task10.py
    used dataset 
    https://www.kaggle.com/datasets/amirnaderiy/smartphone

--- Task10.py
+++ Task10.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('smartphones.csv)
+df = pd.read_csv('smartphones.csv')
                  
-df[df.select_dtypes(include='number') = df.select_dtypes(include='number').fillna(df.mean(numeric_only="True"))
+df[df.select_dtypes(include='number')] = df.select_dtypes(include='number').fillna(df.mean(numeric_only="True"))
 
-for col in df[df.select_dtypes(include='object').columns :
+for col in df[df.select_dtypes(include='object')].columns :
     df[col] = df[col].fillna(df[col].mode()[0])
 
 print("datatype before conversion:")
